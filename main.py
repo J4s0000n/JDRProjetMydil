d46532101a3649d168d1372cef1f944c9371112a
Qj56 <jamesquentin46@gmail.com>
2024-04-17 11:36:24 +0200

    main pour quentin seulement!!!

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+import pygame
+pygame.init()
+pygame.display.set_caption("Oblivion")
+screen=pygame.display.set_mode((500,500))
+
+background = pygame.image.load('PygameAssets-main/Arthur_jdr.png')
+
+running = True
+
+while running:
+    screen.blit(background,(0,0))
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            print("Fermer")
